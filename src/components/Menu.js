d8e8f04641cf5e6a1dd9746525ca361e36bf01a3
Filip Novak <nfilip989@gmail.com>
2025-06-25 12:26:11 +0300

    úprava pozadí

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,42 +1,36 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Menu.css";
- import React, { useState } from 'react';
-import { List, X } from 'react-bootstrap-icons';
+import React from 'react'; 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from "./Logo.js";
+import './Menu.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Menu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(prev => !prev);
-  };
-
   return (
-    <div className="container-fluid mt-3 mr-3 div-menu">
-  <button
-    className="btn btn-primary d-flex align-items-center justify-content-center"
-    type="button"
-    onClick={toggleMenu}
-    aria-expanded={isOpen}
-    aria-controls="menuCollapse"
-    aria-label={isOpen ? 'Close menu' : 'Open menu'}
-  >
-    {isOpen ? <X size={24} /> : <List size={24} />}
-  </button>
-
-  <div
-    className={`collapse ${isOpen ? 'show' : ''}`}
-    id="menuCollapse"
-  >
-    <div className="card card-body mt-3">
-      <ul className="list-unstyled mb-0">
-        <li><a href="#item1" className="d-block py-2">Item 1</a></li>
-        <li><a href="#item2" className="d-block py-2">Item 2</a></li>
-        <li><a href="#item3" className="d-block py-2">Item 3</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
+    <div>
+    <Navbar expand="lg" className="bg-white">
+      <Container>
+        <Navbar.Brand href="/"><Logo/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Domů</Nav.Link>
+            <Nav.Link href="/aboutus">O nás</Nav.Link>
+            <NavDropdown title="Přihlásit se" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/register">Registrovat se</NavDropdown.Item>
+              <NavDropdown.Item href="/login">
+                Přihlásit se
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+   </div>
   );
-};
+}
 
 export default Menu;
