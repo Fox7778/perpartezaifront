124941e6f198ed74bf80e3b56919017755c765e9
Filip Novak <nfilip989@gmail.com>
2025-06-26 16:25:11 +0300

    ůprava chatbota a AIpage

---
+++ src/pages/AIpage.js
@@ -0,0 +1,11 @@
+import Chatbot from "../components/chatbot";
+import React from "react";
+ 
+const AIpage = () => {
+  return (
+    <div className="container py-4">
+      <Chatbot />
+    </div>
+  );
+}
+export default AIpage;
