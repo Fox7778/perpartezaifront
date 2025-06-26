5a27b9e887514e56b2c26ab6d64e8374a9110819
Jakub Líska <liskajakub612@outlook.com>
2025-06-26 18:54:01 +0300

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,27 @@ const Home = () => {
     return (
         <>
         <Container fluid className="bg-light">
-            <Row>
-                <Col>
-                </Col>
+            <Row className="text-center napis1">
+                <h1>Učte se s námi</h1>
             </Row>
         </Container>    
-       
+        <Container fluid className="bg-light">
+            <Row className="text-center text1">
+                <p>
+                    PerpartezAI je inovativní produkt, který si klade za cíl změnit způsob, jakým se studenti učí. <br></br>
+                    Vytvořili jsme webovou aplikaci s umělou inteligencí, která funguje jako osobní doučovatel – dostupný kdykoliv a kdekoliv. Pomáhá studentům lépe porozumět učivu, připravit se na zkoušky a zvládat školu bez stresu.
+                </p>
+            </Row>
+        </Container>
+
+        <Container fluid className="footer">
+            <Row className="d-inline">
+                Kontakt:<a href="mailto:liskajakub612@outlook.com" id="mail">liskajakub612@outlook.com</a>
+            </Row>
+            <Row >
+                &copy; 2025 PerpartezAI, Všechna práva vyhrazena.
+            </Row>
+        </Container>
         </>
     );
 }
